ca661a47cb05745973b008f9e298f8b0d8b66e4a
tomoki.yao <tomoki9226@gmail.com>
2021-03-21 18:53:20 +0900

    action creaters

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+export const searchRepositories = (term: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.SERCH_REPOSITORIES,
+    });
+
+    try {
+      const { data } = await axios.get(
+        'http://registry.npmjs.org/-/v1/search',
+        {
+          params: {
+            text: term,
+          },
+        }
+      );
+
+      const names = data.objects.map((result: any) => {
+        return result.package.name;
+      });
+
+      dispatch({
+        type: ActionType.SERCH_REPOSITORIES_SUCCESS,
+        payload: names,
+      });
+    } catch (err) {
+      dispatch({
+        type: ActionType.SERCH_REPOSITORIES_ERROR,
+        payload: err.message,
+      });
+    }
+  };
+};
